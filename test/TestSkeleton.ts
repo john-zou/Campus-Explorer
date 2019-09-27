7629a1554f6f7ea77c92b19a79f695d55bdd08a3
r6m2b <john.hua.zou@gmail.com>
2019-09-27 14:48:36 -0700

    Use this to create test modules

---
+++ test/TestSkeleton.ts
@@ -0,0 +1,23 @@
+// Module Test Skeleton
+
+// Import Statements
+
+
+
+describe("[TEST CATEGORY NAME]", () => {
+    // Declare common objects as local variables
+
+
+    before(() => {
+        // Runs once, before any "beforeEach"
+
+    });
+
+    beforeEach(() => {
+        // Runs before every test. Happens after "before"
+    });
+
+    it("[TEST DESCRIPTION", async () => {
+        // Test body
+    });
+});
