d49e20a694c621ecac8cad37f14f524a3231d790
r6m2b <john.hua.zou@gmail.com>
2019-09-27 16:19:11 -0700

    IQueryValidator.validate now requires datasetId

--- src/services/IQueryValidator.ts
+++ src/services/IQueryValidator.ts
@@ -1,10 +1,25 @@
 import { InsightDatasetKind } from "../controller/IInsightFacade";
 
+/**
+ * Listed in order of precedence
+ * 
+ * */
 export enum QueryValidationResult {
-    Valid, NotJSON, // todo: add more
+    Valid,
+    NotJSON,
+    MissingBody,
+    MissingOptions,
+    TooManyKeys_Query,
+    WrongType_Body,
+    WrongType_Options,
+    // Too many keys at the query level (i.e. has body, has options, then has st xtra)
+
+    // not really possible but we need this validate takes in "any"
+    // todo: add more
 }
+
 
 export interface IQueryValidator {
     // Takes in a JSON and datasetkind and returns a validation result.
-    validate(json: any, kind: InsightDatasetKind): QueryValidationResult;
+    validate(json: any, datasetId: string, kind: InsightDatasetKind): QueryValidationResult;
 }
--- src/services/QueryValidator.ts
+++ src/services/QueryValidator.ts
@@ -2,7 +2,7 @@ import { IQueryValidator, QueryValidationResult } from "./IQueryValidator";
 import { InsightDatasetKind } from "../controller/IInsightFacade";
 
 export class QueryValidator implements IQueryValidator {
-    validate(json: any, kind: InsightDatasetKind): QueryValidationResult {
+    validate(json: any, datasetId: string, kind: InsightDatasetKind): QueryValidationResult {
 
         throw new Error("Method not implemented.");
     }
