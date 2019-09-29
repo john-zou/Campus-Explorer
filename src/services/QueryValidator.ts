b0b0746e67b452966e8630108aa7f617bff39a6c
r6m2b <john.hua.zou@gmail.com>
2019-09-28 22:20:38 -0700

    unfinished implementation

--- src/services/QueryValidator.ts
+++ src/services/QueryValidator.ts
@@ -1,9 +1,19 @@
-import { IQueryValidator, QueryValidationResult } from "./IQueryValidator";
+import { IQueryValidator, QueryValidationResult as R, QueryValidationResultFlag as F }
+    from "./IQueryValidator";
 import { InsightDatasetKind } from "../controller/IInsightFacade";
 
 export class QueryValidator implements IQueryValidator {
-    public validate(json: any, datasetIds: string[], kind: InsightDatasetKind): QueryValidationResult {
+    public validate(json: any, datasetIds: string[], kind: InsightDatasetKind): R {
+        if (this.isMissingKey(json, "BODY")) {
+            return new R(F.MissingBody, null);
+        }
+        if (this.isMissingKey(json, "OPTIONS")) {
+            return new R(F.MissingOptions, null);
+        }
+        // Next: Too many keys, etc.
+    }
 
-        throw new Error("Method not implemented.");
+    private isMissingKey(json: any, key: string): boolean{
+        return !Object.keys(json).includes(key);
     }
 }
