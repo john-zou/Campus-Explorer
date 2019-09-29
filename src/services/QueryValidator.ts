2154af1c9059677e1d4c91af9e8670fd14680d2d
SS <SS@SS.SS>
2019-09-28 23:31:15 -0700

    Update QueryValidator.ts

--- src/services/QueryValidator.ts
+++ src/services/QueryValidator.ts
@@ -1,19 +1,31 @@
-import { IQueryValidator, QueryValidationResult as R, QueryValidationResultFlag as F }
-    from "./IQueryValidator";
+import { IQueryValidator, QueryValidationResult as R, QueryValidationResultFlag as F } from "./IQueryValidator";
 import { InsightDatasetKind } from "../controller/IInsightFacade";
+import { isObject } from "util";
 
 export class QueryValidator implements IQueryValidator {
     public validate(json: any, datasetIds: string[], kind: InsightDatasetKind): R {
+        // Highest level checks
         if (this.isMissingKey(json, "BODY")) {
-            return new R(F.MissingBody, null);
+            return new R(F.MissingBody);
         }
         if (this.isMissingKey(json, "OPTIONS")) {
-            return new R(F.MissingOptions, null);
+            return new R(F.MissingOptions);
         }
-        // Next: Too many keys, etc.
+        if (this.hasTooManyKeys(json, 2)) {
+            return new R(F.TooManyKeys_Query);
+        }
+        // WrongType_Body
+        const body = json.BODY;
+        if (body == null || typeof body !== "object") {
+            return new R(F.WrongType_Body);
+        }
     }
 
-    private isMissingKey(json: any, key: string): boolean{
+    private hasTooManyKeys(json: any, max: number) {
+        return Object.keys(json).length > max;
+    }
+
+    private isMissingKey(json: any, key: string): boolean {
         return !Object.keys(json).includes(key);
     }
 }
