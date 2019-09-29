a83fd15e72ae83deff2d9356bfcf891fd1799e10
SS <SS@SS.SS>
2019-09-29 00:13:42 -0700

    implementation in progress

---
+++ src/query_schema/IQuery.ts
@@ -0,0 +1,37 @@
+// export interface IQuery {
+//     WHERE: IAnd|IOr|ILt|IGt|IEq|IIs|INot;
+//     OPTIONS: IOptions;
+// }
+//
+// export interface IOptions {
+//     COLUMNS: string[];
+//     ORDER: string;
+// }
+//
+// export interface IAnd {
+//     AND: Array<IAnd|IOr|ILt|IGt|IEq|IIs|INot>;
+// }
+//
+// export interface IOr {
+//     OR: Array<IAnd|IOr|ILt|IGt|IEq|IIs|INot>;
+// }
+//
+// export interface INot {
+//     NOT: IAnd|IOr|ILt|IGt|IEq|IIs|INot;
+// }
+//
+// export interface ILt {
+//
+// }
+//
+// export interface IGt {
+//
+// }
+//
+// export interface IEq {
+//
+// }
+//
+// export interface IIs {
+//
+// }
--- src/services/QueryValidator.ts
+++ src/services/QueryValidator.ts
@@ -1,11 +1,12 @@
 import { IQueryValidator, QueryValidationResult as R, QueryValidationResultFlag as F } from "./IQueryValidator";
 import { InsightDatasetKind } from "../controller/IInsightFacade";
 import { isObject } from "util";
+import { NotImplementedError } from "restify";
 
 export class QueryValidator implements IQueryValidator {
     public validate(json: any, datasetIds: string[], kind: InsightDatasetKind): R {
         // Highest level checks
-        if (this.isMissingKey(json, "BODY")) {
+        if (this.isMissingKey(json, "WHERE")) {
             return new R(F.MissingBody);
         }
         if (this.isMissingKey(json, "OPTIONS")) {
@@ -14,11 +15,22 @@ export class QueryValidator implements IQueryValidator {
         if (this.hasTooManyKeys(json, 2)) {
             return new R(F.TooManyKeys_Query);
         }
-        // WrongType_Body
-        const body = json.BODY;
-        if (body == null || typeof body !== "object") {
+        // WrongType_Body -- BODY in EBNF is property WHERE
+        const where = json.WHERE;
+        if (where == null || typeof where !== "object") {
             return new R(F.WrongType_Body);
         }
+        // WrongType_Options
+        const options = json.OPTIONS;
+        if (options == null || typeof options !== "object") {
+            return new R(F.WrongType_Options);
+        }
+        // "WHERE" must be a good filter
+        const filterValidationResult = this.validateFilter(where, datasetIds);
+    }
+
+    private validateFilter(filter: any, datasetIds: string[]) {
+        throw new NotImplementedError();
     }
 
     private hasTooManyKeys(json: any, max: number) {
