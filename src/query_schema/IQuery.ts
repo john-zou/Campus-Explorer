44bb5038438a7f289a0435951ae50f631eab78b4
r6m2b <john.hua.zou@gmail.com>
2019-09-29 20:38:32 -0700

    Awakening

--- src/query_schema/IQuery.ts
+++ src/query_schema/IQuery.ts
@@ -1,37 +1,41 @@
-// export interface IQuery {
-//     WHERE: IAnd|IOr|ILt|IGt|IEq|IIs|INot;
-//     OPTIONS: IOptions;
-// }
-//
-// export interface IOptions {
-//     COLUMNS: string[];
-//     ORDER: string;
-// }
-//
-// export interface IAnd {
-//     AND: Array<IAnd|IOr|ILt|IGt|IEq|IIs|INot>;
-// }
-//
-// export interface IOr {
-//     OR: Array<IAnd|IOr|ILt|IGt|IEq|IIs|INot>;
-// }
-//
-// export interface INot {
-//     NOT: IAnd|IOr|ILt|IGt|IEq|IIs|INot;
-// }
-//
-// export interface ILt {
-//
-// }
-//
-// export interface IGt {
-//
-// }
-//
-// export interface IEq {
-//
-// }
-//
-// export interface IIs {
-//
-// }
+export interface IQuery {
+    WHERE: IAnd|IOr|ILt|IGt|IEq|IIs|INot;
+    OPTIONS: IOptionsWithoutOrder | IOptionsWithOrder;
+}
+
+export interface IOptionsWithOrder {
+    COLUMNS: string[];
+    ORDER: string;
+}
+
+export interface IOptionsWithoutOrder {
+    COLUMNS: string[];
+}
+
+export interface IAnd {
+    AND: Array<IAnd|IOr|ILt|IGt|IEq|IIs|INot>;
+}
+
+export interface IOr {
+    OR: Array<IAnd|IOr|ILt|IGt|IEq|IIs|INot>;
+}
+
+export interface INot {
+    NOT: IAnd|IOr|ILt|IGt|IEq|IIs|INot;
+}
+
+export interface ILt {
+    LT: any;
+}
+
+export interface IGt {
+    GT: any;
+}
+
+export interface IEq {
+    EQ: any;
+}
+
+export interface IIs {
+    IS: any;
+}
