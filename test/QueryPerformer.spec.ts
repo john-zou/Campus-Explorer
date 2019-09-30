d5efb77dd127af3ab8898cb2303b5b784b1dea9b
Owen Sheekey <oisheekey@gmail.com>
2019-09-29 19:17:48 -0700

    additions to testing

--- test/QueryPerformer.spec.ts
+++ test/QueryPerformer.spec.ts
@@ -4,9 +4,10 @@ import { InsightDatasetKind } from "../src/controller/IInsightFacade";
 import { DatasetManager } from "../src/services/DatasetManager";
 import { QueryPerformer } from "../src/services/QueryPerformer";
 
-describe("OrderData", () => {
+describe("Performs Queries", () => {
+    // ONLY tests valid queries
     // Constructing a new IParsedData to use in testing
-    class IParsedDataT implements IParsedData {
+    class ParsedDataT implements IParsedData {
         public data: [{ courses_dept: "epse", courses_avg: 97.09 },
                       { courses_dept: "math", courses_avg: 97.09 },
                       { courses_dept: "nurs", courses_avg: 98.21 },
@@ -18,19 +19,33 @@ describe("OrderData", () => {
         public kind: InsightDatasetKind.Courses;
         public numRows: 2;
     }
-    let dataset: IParsedDataT;
+    let query: any = {
+            WHERE: {
+               GT: {
+                  courses_avg: 97
+               }
+            },
+            OPTIONS: {
+                COLUMNS: [
+                   "courses_dept",
+                   "courses_avg"
+                ],
+                ORDER: "courses_avg"
+            }
+    };
+
+    let dataset: ParsedDataT;
     let qp: QueryPerformer;
     before(() => {
         // Runs once, before any "beforeEach"
     });
 
     beforeEach(() => {
-        dataset = new IParsedDataT();
+        dataset = new ParsedDataT();
         qp = new QueryPerformer();
     });
 
     it("Should properly order a dataset", async () => {
-        // Note that an 'order' query will just be a string
-        let order: string = "courses_avg";
+        // qp.performQuery
     });
 });
