f361e5ad4cc2b5a6f6b8e56756e126bc93fa334f
Owen Sheekey <oisheekey@gmail.com>
2019-09-29 21:43:30 -0700

    more changes to QP and its tests

--- test/QueryPerformer.spec.ts
+++ test/QueryPerformer.spec.ts
@@ -6,36 +6,34 @@ import { QueryPerformer } from "../src/services/QueryPerformer";
 import TestUtil from "./TestUtil";
 import Log from "../src/Util";
 import { IQueryValidator, QueryValidationResultFlag, QueryValidationResult } from "../src/services/IQueryValidator";
+import { IQuery, IGt, IFilter, IOptions } from "../src/query_schema/IQuery";
 
 describe("Perform Query Tests", () => {
     // ONLY tests valid queries
     // Constructing a new IParsedData to use in testing
     class ParsedDataT implements IParsedData {
-        public data: [{ courses_dept: "epse", courses_avg: 97.19 },
-                      { courses_dept: "math", courses_avg: 97.09 },
-                      { courses_dept: "nurs", courses_avg: 98.21 },
-                      { courses_dept: "epse", courses_avg: 97.08 },
-                      { courses_dept: "cnps", courses_avg: 97.47 },
-                      { courses_dept: "math", courses_avg: 97.25 },
-                      { courses_dept: "spph", courses_avg: 98.98 }];
-        public id: "test";
+        public data: any[];
+        public id: string;
         public kind: InsightDatasetKind.Courses;
-        public numRows: 2;
+        public numRows: number;
+
+        public constructor(data: any[], id: string, numRows: number) {
+            this.data = data;
+            this.id = id;
+            this.kind = InsightDatasetKind.Courses;
+            this.numRows = numRows;
+        }
     }
-    let query: any = {
-            WHERE: {
-               GT: {
-                  courses_avg: 97
-               }
-            },
-            OPTIONS: {
-                COLUMNS: [
-                   "courses_dept",
-                   "courses_avg"
-                ],
-                ORDER: "courses_avg"
-            }
-    };
+
+    class QueryT implements IQuery {
+        public WHERE: IFilter;
+        public OPTIONS: IOptions;
+
+        public constructor(where: IFilter, options: IOptions) {
+            this.WHERE = where;
+            this.OPTIONS = options;
+        }
+    }
 
     class QueryValidatorT implements IQueryValidator {
         public validate(json: any, datasetIds: string[], kind: InsightDatasetKind): QueryValidationResult {
@@ -50,17 +48,35 @@ describe("Perform Query Tests", () => {
     });
 
     beforeEach(() => {
-        let temp: ParsedDataT = new ParsedDataT();
+        let temp: ParsedDataT = new ParsedDataT([{ courses_dept: "epse", courses_avg: 97.19 },
+                                                 { courses_dept: "math", courses_avg: 97.09 },
+                                                 { courses_dept: "nurs", courses_avg: 98.21 },
+                                                 { courses_dept: "epse", courses_avg: 97.08 },
+                                                 { courses_dept: "cnps", courses_avg: 97.47 },
+                                                 { courses_dept: "math", courses_avg: 97.25 },
+                                                 { courses_dept: "spph", courses_avg: 98.98 }],
+                                                 "test", 2);
         dataset = [temp];
         qp = new QueryPerformer(new QueryValidatorT());
     });
 
-    it("Should properly order a dataset", async () => {
-        Log.trace(query["OPTIONS"]);
+    it("Should query a dataset with no order", async () => {
+        let query: IQuery = new QueryT({GT: {courses_avg: 97}},
+            {COLUMNS : ["courses_dept", "courses_avg"]});
         let found: any[] = await qp.performQuery(query, dataset, ["courses"]);
-        for (let f of found) {
-            Log.trace(f.courses_dept);
-        }
+        expect(found).to.deep.equal([{ courses_dept: "epse", courses_avg: 97.19 },
+                                     { courses_dept: "math", courses_avg: 97.09 },
+                                     { courses_dept: "nurs", courses_avg: 98.21 },
+                                     { courses_dept: "epse", courses_avg: 97.08 },
+                                     { courses_dept: "cnps", courses_avg: 97.47 },
+                                     { courses_dept: "math", courses_avg: 97.25 },
+                                     { courses_dept: "spph", courses_avg: 98.98 }]);
+    });
+
+    it("Should properly order a dataset", async () => {
+        let query: IQuery = new QueryT({GT: {courses_avg: 97}},
+            {COLUMNS : ["courses_dept", "courses_avg"], ORDER: "courses_avg"});
+        let found: any[] = await qp.performQuery(query, dataset, ["courses"]);
         expect(found).to.deep.equal([{ courses_dept: "epse", courses_avg: 97.08 },
                                      { courses_dept: "math", courses_avg: 97.09 },
                                      { courses_dept: "epse", courses_avg: 97.19 },
