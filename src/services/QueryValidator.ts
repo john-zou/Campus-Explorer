23ebca68be6600a5f631fcc62b26f56262bffb95
r6m2b <john.hua.zou@gmail.com>
2019-09-29 18:06:53 -0700

    Update QueryValidator.ts

--- src/services/QueryValidator.ts
+++ src/services/QueryValidator.ts
@@ -3,6 +3,7 @@ import { InsightDatasetKind } from "../controller/IInsightFacade";
 import { isObject } from "util";
 import { NotImplementedError } from "restify";
 import { mfields as MFields } from "../query_schema/MFields";
+import { sfields as SFields } from "../query_schema/SFields";
 
 export class QueryValidator implements IQueryValidator {
     public validate(json: any, datasetIds: string[], kind: InsightDatasetKind): R {
@@ -26,8 +27,60 @@ export class QueryValidator implements IQueryValidator {
         if (options == null || typeof options !== "object") {
             return new R(F.WrongType_Options);
         }
-        // "WHERE" must be a good filter
-        const filterValidationResult: [F, string] = this.validateFilter(where, datasetIds);
+        // if "WHERE" is not just { }
+        let whereMentionsId: boolean = false;
+        let id: string = "";
+        if (Object.keys(where).length !== 0) {
+            // "WHERE" must be a good filter
+            const filterValidationResult: [F, string] = this.validateFilter(where, datasetIds);
+            const filterValidationFlag: F = filterValidationResult[0];
+            if (filterValidationFlag !== F.Valid) {
+                return new R(filterValidationFlag);
+            }
+            whereMentionsId = true;
+            id = filterValidationResult[1]; // Remember the ID to compare with options
+        }
+        // Validate Options
+        const optionsValidationResult: [F, string] = this.validateOptions(options, datasetIds);
+        const optionsValidationFlag: F = optionsValidationResult[0];
+        if (optionsValidationFlag !== F.Valid) {
+            return new R(optionsValidationFlag);
+        }
+        const idFromOptions: string = optionsValidationResult[1];
+        if (whereMentionsId) {
+            if (id !== idFromOptions) {
+                return new R(F.MoreThanOneId);
+            }
+        }
+        return new R(F.Valid, idFromOptions);
+    }
+
+    private validateOptions(options: any, datasetIds: string[]): [F, string] {
+        // Check that options is an object
+        if (options == null || typeof options !== "object") {
+            return [F.WrongType_Options, null];
+        }
+        // Check that options contains column key
+        if (Object.keys(options).length === 0) {
+            return [F.MissingColumns, null];
+        }
+        if (!Object.keys(options).includes("COLUMNS")) {
+            return [F.MissingColumns, null];
+        }
+        // Check whether options contains order key
+        const orderExists: boolean = Object.keys(options).includes("ORDER");
+        // Check for too many keys in options
+        if (orderExists) {
+            // Ensure there are no more than 2 keys
+            if (this.hasTooManyKeys(options, 2)) {
+                return [F.TooManyKeys_Options, null];
+            }
+        } else {
+            if (this.hasTooManyKeys(options, 1)) {
+                return [F.TooManyKeys_Options, null];
+            }
+        }
+        // TODO: check the columns and order (make sure order is in columns)
     }
 
     private validateFilterArray(filters: any[], datasetIds: string[]): [F, string] {
@@ -35,6 +88,8 @@ export class QueryValidator implements IQueryValidator {
         if (!filters || filters.length === 0) {
             return [F.WrongType_LogicComparison, null];
         }
+        // Essentially, folds the validateFilter results into one, making sure they are
+        // all valid and all have the same ID.
         let result: [F, string] = this.validateFilter(filters[0], datasetIds);
         if (result[0] !== F.Valid) {
             return result;
@@ -44,7 +99,7 @@ export class QueryValidator implements IQueryValidator {
             if (nextResult[0] !== F.Valid) {
                 return nextResult;
             }
-            if (nextResult[1] !== result[1]) {
+            if (nextResult[1] !== result[1]) { // verify they have the same ID
                 return [F.MoreThanOneId, ""];
             }
         }
@@ -77,7 +132,33 @@ export class QueryValidator implements IQueryValidator {
     }
 
     private validateSComparison(sc: any, datasetIds: string[]): [F, string] {
-        throw new NotImplementedError();
+        if (sc == null || typeof sc !== "object") {
+            return [F.WrongType_SComparison, null];
+        }
+        if (this.hasTooManyKeys(sc, 1)) {
+            return [F.TooManyKeys_SComparison, null];
+        }
+        const key: string = Object.keys(sc)[0];
+        const parseResult: [F, string, string] = this.parseKeystring(key);
+        const parseResultFlag: F = parseResult[0];
+        if (parseResultFlag !== F.Valid) {
+            return [parseResultFlag, ""];
+        }
+        const id: string = parseResult[1];
+        if (!datasetIds.includes(id)) {
+            return [F.IdDoesNotExist, ""];
+        }
+        const sfield: string = parseResult[2];
+        if (!SFields.includes(sfield)) {
+            return [F.InvalidSField, ""];
+        }
+        // Validate the value -- it must be a number
+        const value: any = Object.values(sc)[0];
+        if (value == null || typeof value !== "number") {
+            return [F.MValueNotANumber, ""];
+        }
+        // It's valid!
+        return [F.Valid, id];
     }
 
     private validateMComparison(mc: any, datasetIds: string[]): [F, string] {
@@ -89,16 +170,16 @@ export class QueryValidator implements IQueryValidator {
             return [F.TooManyKeys_MComparison, null];
         }
         const key: string = Object.keys(mc)[0];
-        const parseResult = this.parseKeystring(key);
-        const parseResultFlag = parseResult[0];
+        const parseResult: [F, string, string] = this.parseKeystring(key);
+        const parseResultFlag: F = parseResult[0];
         if (parseResultFlag !== F.Valid) {
             return [parseResultFlag, ""];
         }
-        const id = parseResult[1];
+        const id: string = parseResult[1];
         if (!datasetIds.includes(id)) {
             return [F.IdDoesNotExist, ""];
         }
-        const mfield = parseResult[2];
+        const mfield: string = parseResult[2];
         if (!MFields.includes(mfield)) {
             return [F.InvalidMfield, ""];
         }
