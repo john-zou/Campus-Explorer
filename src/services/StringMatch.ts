41fe7a98feb754595858c4c94725e7dfb833baeb
r6m2b <john.hua.zou@gmail.com>
2019-10-03 22:09:43 -0700

    redundant

--- src/services/StringMatch.ts
+++ src/services/StringMatch.ts
@@ -1,33 +1,33 @@
 
-export function isMatch(stringFromData: string, inputStringFromQuery: string,
-                        prefixAsterisk: boolean, postfixAsterisk: boolean): boolean {
-    // see if inputString is in field
-    if (!prefixAsterisk && !postfixAsterisk) {
-        return stringFromData === inputStringFromQuery;
-    }
-    if (!prefixAsterisk && postfixAsterisk) {
-        if (stringFromData.length < inputStringFromQuery.length) {
-            return false;
-        }
-        for (let i = 0; i < inputStringFromQuery.length; ++i) {
-            if (stringFromData[i] !== inputStringFromQuery[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-    if (prefixAsterisk && !postfixAsterisk) {
-        if (stringFromData.length < inputStringFromQuery.length) {
-            return false;
-        }
-        for (let i = inputStringFromQuery.length - 1, j = stringFromData.length - 1; i >= 0; --i, --j) {
-            if (inputStringFromQuery[i] !== inputStringFromQuery[j]) {
-                return false;
-            }
-        }
-        return true;
-    } else {
-        // Both prefix and postfix asterisk
-        return stringFromData.includes(inputStringFromQuery);
-    }
-}
+// export function isMatch(stringFromData: string, inputStringFromQuery: string,
+//                         prefixAsterisk: boolean, postfixAsterisk: boolean): boolean {
+//     // see if inputString is in field
+//     if (!prefixAsterisk && !postfixAsterisk) {
+//         return stringFromData === inputStringFromQuery;
+//     }
+//     if (!prefixAsterisk && postfixAsterisk) {
+//         if (stringFromData.length < inputStringFromQuery.length) {
+//             return false;
+//         }
+//         for (let i = 0; i < inputStringFromQuery.length; ++i) {
+//             if (stringFromData[i] !== inputStringFromQuery[i]) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+//     if (prefixAsterisk && !postfixAsterisk) {
+//         if (stringFromData.length < inputStringFromQuery.length) {
+//             return false;
+//         }
+//         for (let i = inputStringFromQuery.length - 1, j = stringFromData.length - 1; i >= 0; --i, --j) {
+//             if (inputStringFromQuery[i] !== inputStringFromQuery[j]) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     } else {
+//         // Both prefix and postfix asterisk
+//         return stringFromData.includes(inputStringFromQuery);
+//     }
+// }
