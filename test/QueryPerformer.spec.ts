a4cb84b6325f96f302785b4d4e072317d0ac31c8
Owen Sheekey <oisheekey@gmail.com>
2019-10-03 19:02:56 -0700

    last few tests

--- test/QueryPerformer.spec.ts
+++ test/QueryPerformer.spec.ts
@@ -9,86 +9,7 @@ import { IQueryValidator, QueryValidationResultFlag, QueryValidationResult } fro
 import { IQuery, IGt, IFilter, IOptions } from "../src/query_schema/IQuery";
 import { orderData, whereFilter } from "../src/services/QueryPerformerFunctions";
 
-// describe("Perform Query Tests", () => {
-//     // ONLY tests valid queries
-//     // Constructing a new IParsedData to use in testing
-//     class ParsedDataT implements IParsedData {
-//         public data: any[];
-//         public id: string;
-//         public kind: InsightDatasetKind.Courses;
-//         public numRows: number;
-
-//         public constructor(data: any[], id: string, numRows: number) {
-//             this.data = data;
-//             this.id = id;
-//             this.kind = InsightDatasetKind.Courses;
-//             this.numRows = numRows;
-//         }
-//     }
-
-//     class QueryT implements IQuery {
-//         public WHERE: IFilter;
-//         public OPTIONS: IOptions;
-
-//         public constructor(where: IFilter, options: IOptions) {
-//             this.WHERE = where;
-//             this.OPTIONS = options;
-//         }
-//     }
-
-//     class QueryValidatorT implements IQueryValidator {
-//         public validate(json: any, datasetIds: string[], kind: InsightDatasetKind): QueryValidationResult {
-//             return new QueryValidationResult(QueryValidationResultFlag.Valid, "test");
-//         }
-//     }
-
-//     let dataset: ParsedDataT[];
-//     let qp: QueryPerformer;
-//     before(() => {
-//         // Runs once, before any "beforeEach"
-//     });
-
-//     beforeEach(() => {
-//         let temp: ParsedDataT = new ParsedDataT([{ courses_dept: "epse", courses_avg: 97.19 },
-//                                                  { courses_dept: "math", courses_avg: 97.09 },
-//                                                  { courses_dept: "nurs", courses_avg: 98.21 },
-//                                                  { courses_dept: "epse", courses_avg: 97.08 },
-//                                                  { courses_dept: "cnps", courses_avg: 97.47 },
-//                                                  { courses_dept: "math", courses_avg: 97.25 },
-//                                                  { courses_dept: "spph", courses_avg: 98.98 }],
-//                                                  "test", 2);
-//         dataset = [temp];
-//         qp = new QueryPerformer(new QueryValidatorT());
-//     });
-
-//     it("Should query a dataset with no order", async () => {
-//         let query: IQuery = new QueryT({GT: {courses_avg: 97}},
-//             {COLUMNS : ["courses_dept", "courses_avg"]});
-//         let found: any[] = await qp.performQuery(query, dataset, ["courses"]);
-//         expect(found).to.deep.equal([{ courses_dept: "epse", courses_avg: 97.19 },
-//                                      { courses_dept: "math", courses_avg: 97.09 },
-//                                      { courses_dept: "nurs", courses_avg: 98.21 },
-//                                      { courses_dept: "epse", courses_avg: 97.08 },
-//                                      { courses_dept: "cnps", courses_avg: 97.47 },
-//                                      { courses_dept: "math", courses_avg: 97.25 },
-//                                      { courses_dept: "spph", courses_avg: 98.98 }]);
-//     });
-
-//     it("Should properly order a dataset", async () => {
-//         let query: IQuery = new QueryT({GT: {courses_avg: 97}},
-//             {COLUMNS : ["courses_dept", "courses_avg"], ORDER: "courses_avg"});
-//         let found: any[] = await qp.performQuery(query, dataset, ["courses"]);
-//         expect(found).to.deep.equal([{ courses_dept: "epse", courses_avg: 97.08 },
-//                                      { courses_dept: "math", courses_avg: 97.09 },
-//                                      { courses_dept: "epse", courses_avg: 97.19 },
-//                                      { courses_dept: "math", courses_avg: 97.25 },
-//                                      { courses_dept: "cnps", courses_avg: 97.47 },
-//                                      { courses_dept: "nurs", courses_avg: 98.21 },
-//                                      { courses_dept: "spph", courses_avg: 98.98 }]);
-//     });
-// });
-
-describe("OrderData testing", () => {
+describe("QueryPerformerFunctions : orderData", () => {
     class QueryT implements IQuery {
         public WHERE: IFilter;
         public OPTIONS: IOptions;
@@ -150,7 +71,7 @@ describe("OrderData testing", () => {
     });
 });
 
-describe("filterWhere testing", () => {
+describe("QueryPerformerFunctions : filterWhere", () => {
 
     before(() => {
         // Runs once, before any "beforeEach"
@@ -207,11 +128,24 @@ describe("filterWhere testing", () => {
         result = whereFilter({ courses_dept: "epse", courses_avg: 90 }, where);
         expect(result).to.equal(false);
         // Case 3, reject
-        result = whereFilter({ courses_dept: "cpsc", courses_avg: 96 }, where);
+        result = whereFilter({ courses_dept: "cpsc", courses_avg: 99 }, where);
         expect(result).to.equal(false);
     });
 
     it("Should correctly apply OR", async () => {
-        // TODO
+        let where: any = {OR: [{GT: { courses_avg: 96}},
+            {IS: { courses_dept: "epse"}}]};
+        // Case 1, accept
+        let result: boolean = whereFilter({ courses_dept: "epse", courses_avg: 99 }, {EQ: {courses_avg: 99}});
+        expect(result).to.equal(true);
+        // Case 2, accept
+        result = whereFilter({ courses_dept: "epse", courses_avg: 90 }, where);
+        expect(result).to.equal(true);
+        // Case 3, accept
+        result = whereFilter({ courses_dept: "cpsc", courses_avg: 99 }, where);
+        expect(result).to.equal(true);
+        // Case 4, reject
+        result = whereFilter({ courses_dept: "cpsc", courses_avg: 90 }, where);
+        expect(result).to.equal(false);
     });
 });
