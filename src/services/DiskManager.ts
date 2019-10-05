be0608c1ffa61ee8b151c862c9b1ed45bc51dd4e
Owen Sheekey <oisheekey@gmail.com>
2019-10-05 13:42:04 -0700

    implemented saveDataset

--- src/services/DiskManager.ts
+++ src/services/DiskManager.ts
@@ -1,19 +1,26 @@
 import { IDiskManager } from "./IDiskManager";
 import { IParsedData } from "../data/IParsedData";
+import fs = require("fs");
 
 export class DiskManager implements IDiskManager {
-    // Constructor creates disk .txt file
-    // if it does not already exist
-    public constructor() {
-        // Check to see if file already exists
-        throw new Error("Method not implemented.");
-    }
+    // Constant
+    private FILE: string = "./db.txt";
 
     public saveDataset(dataset: IParsedData): Promise<void> {
         // Convert IParsedData into a nice JSON
         const dataAsJSON: string = JSON.stringify(dataset);
-        // Save JSON into an (already existing) .txt file
-        throw new Error("Method not implemented.");
+        // Save JSON into a .txt file
+        // Note that saveDataset should not be called more than once
+        // before promise returns
+        return new Promise ((resolve, reject) => {
+            fs.writeFile(this.FILE, dataAsJSON, (err: any) => {
+                if (err.isInstanceOf(Error)) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            });
+        });
     }
 
     public deleteDataset(id: string): Promise<string> {
