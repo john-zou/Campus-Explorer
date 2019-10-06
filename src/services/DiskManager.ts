7b7e48a3280340918abfe4b78d040a9fc1899800
Owen Sheekey <oisheekey@gmail.com>
2019-10-06 00:16:39 -0700

    Update DiskManager.ts

--- src/services/DiskManager.ts
+++ src/services/DiskManager.ts
@@ -16,7 +16,7 @@ export class DiskManager implements IDiskManager {
             return;
         }
         // Check if database directory already exists
-        const dirs: string[] = await fs.promises.readdir("./");
+        const dirs: string[] =  fs.readdirSync("./");
         if (!dirs.includes(this.DIRNAME)) {
             // Makes new directory if it does not already exist
             await fs.promises.mkdir(this.FILEDIR);
@@ -30,30 +30,30 @@ export class DiskManager implements IDiskManager {
         // Save JSON into a .txt file
         // Note that saveDataset should not be called more than once
         // before promise returns
-        await fs.promises.writeFile(this.FILEDIR + dataset.id, dataAsJSON);
+        fs.writeFileSync(this.FILEDIR + dataset.id, dataAsJSON);
     }
 
     // this may not be the most effecient implementation
     public async deleteDataset(id: string): Promise<void> {
         // Make sure file not already in dataset
-        const fileNames: string[] = await fs.promises.readdir(this.FILEDIR);
+        const fileNames: string[] = fs.readdirSync(this.FILEDIR);
         if (!fileNames.includes(id)) {
             throw new Error("Cannot delete file with id: " + id + " because it does not exist");
         }
         // Delete file
-        await fs.promises.unlink(this.FILEDIR + id);
+        fs.unlinkSync(this.FILEDIR + id);
     }
 
     public async getDatasets(): Promise<IParsedData[]> {
         let fileNames: string[] = [];
-        fileNames = await fs.promises.readdir(this.FILEDIR);
+        fileNames = fs.readdirSync(this.FILEDIR);
         let foundData: IParsedData[] = [];
         // Terminate early if no datasets to get
         if (fileNames === undefined || fileNames.length === 0) {
             return foundData;
         }
         for (const file of fileNames) {
-            foundData.push(JSON.parse((await fs.promises.readFile(this.FILEDIR + file)).toString()));
+            foundData.push(JSON.parse((fs.readFileSync(this.FILEDIR + file)).toString()));
         }
         this.growUp();
         return foundData;
