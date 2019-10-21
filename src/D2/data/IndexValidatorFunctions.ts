187902dd15d86b386bf9a768d80f281f16cb99b7
Owen Sheekey <oisheekey@gmail.com>
2019-10-20 21:14:15 -0700

    support for multiple tables in html

--- src/D2/data/IndexValidatorFunctions.ts
+++ src/D2/data/IndexValidatorFunctions.ts
@@ -1,14 +1,22 @@
-export function tablesearch(doc: Document): Document {
+// Needs to be restructured to return a list of tables
+// May be a good idea to do some actual parsing on the table
+export function tablesearch(doc: Document): Document[] {
     if (doc.nodeName === "table") {
-        return doc;
+        return [doc];
     }
-    for (const cnode in document.childNodes) {
+    let docs: Document[] = [];
+    for (const i in doc.childNodes) {
+        const cnode = doc.childNodes[i];
         // Call table search recursively
         const possibleTable: Document = this.tablesearch(cnode);
         // Check if table found in recursive call
-        if (possibleTable != null) {
-            return possibleTable;
+        if (possibleTable !== null) {
+            docs.push(possibleTable);
         }
     }
+    if (docs !== []) {
+        return docs;
+    }
+    // Nothing found
     return null;
 }
