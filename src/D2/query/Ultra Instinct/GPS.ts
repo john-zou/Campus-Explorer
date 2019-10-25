7c3835f7bac972820d1321af23a7ee9ebfe85684
John Zou <huazou@students.cs.ubc.ca>
2019-10-24 20:33:48 -0700

    spacing

--- src/D2/query/Ultra Instinct/GPS.ts
+++ src/D2/query/Ultra Instinct/GPS.ts
@@ -10,31 +10,31 @@ export const getLatLonFromAddress = (address: string): Promise<GeoResponse> =>  
     return new Promise<GeoResponse> ((resolve, reject) => {
         // https://nodejs.org/docs/latest-v10.x/api/http.html#http_http_get_options_callback
         http.get(DDOS_TARGET + formattedAddress, (res: any) => {
-        const { statusCode } = res;
-        const contentType = res.headers["content-type"];
+            const { statusCode } = res;
+            const contentType = res.headers["content-type"];
 
-        let error;
-        if (statusCode !== 200 && statusCode !== 404) {
-            error = new Error("Request Failed.\n" +
-                      `Status Code: ${statusCode}`);
-        } else if (!/^application\/json/.test(contentType)) {
-            error = new Error("Invalid content-type.\n" +
-                      "Expected application/json but received ${contentType}");
-        }
-        if (error) {
-            Log.error(error.message);
-            // consume response data to free up memory
-            res.resume();
-            reject(error);
-            return;
-        }
+            let error;
+            if (statusCode !== 200 && statusCode !== 404) {
+                error = new Error("Request Failed.\n" +
+                          `Status Code: ${statusCode}`);
+            } else if (!/^application\/json/.test(contentType)) {
+                error = new Error("Invalid content-type.\n" +
+                          "Expected application/json but received ${contentType}");
+            }
+            if (error) {
+                Log.error(error.message);
+                // consume response data to free up memory
+                res.resume();
+                reject(error);
+                return;
+            }
 
-        res.setEncoding("utf8");
-        let rawData = "";
-        res.on("data", (chunk: any) => {
-            rawData += chunk;
-        });
-        res.on("end", () => {
+            res.setEncoding("utf8");
+            let rawData = "";
+            res.on("data", (chunk: any) => {
+                rawData += chunk;
+            });
+            res.on("end", () => {
                 try {
                     const parsedData = JSON.parse(rawData);
                     resolve(parsedData as GeoResponse);
