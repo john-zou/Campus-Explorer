62e86a0869b56ef35529fafeea310f171722f5db
Owen Sheekey <oisheekey@gmail.com>
2019-10-24 20:16:12 -0700

    Create IRoom.ts

---
+++ src/D2/data/IRoom.ts
@@ -0,0 +1,13 @@
+export interface IRoom {
+    fullname: string;
+    shortname: string;
+    number: string;
+    name: string;
+    address: string;
+    lat: number;
+    lon: number;
+    seats: number;
+    type: string;
+    furniture: string;
+    href: string;
+}
