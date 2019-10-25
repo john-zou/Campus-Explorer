81389eb3579e7856c3c84a642cf19a6f5969034a
Owen Sheekey <oisheekey@gmail.com>
2019-10-24 20:15:40 -0700

    adding new functions

--- src/D2/data/IndexValidator.ts
+++ src/D2/data/IndexValidator.ts
@@ -3,17 +3,18 @@ const Parse5 = require("parse5");
 import JSZip = require("jszip");
 import { tablesearch } from "./IndexValidatorFunctions";
 import { InsightError } from "../../controller/IInsightFacade";
+import { IRoom } from "./IRoom";
 
 export class IndexValidator {
     public async validate(index: JSZip.JSZipObject): Promise<Room[]> {
         const parsedDoc: Document = Parse5.parse(await index.async("text"));
         // traverse document recursively looking for a table element
-        const tables: Document[] = tablesearch(document);
+        const tables: ChildNode[] = tablesearch(document);
         if (tables === null) {
             // fail
             throw new InsightError("No table found in index.htm");
         }
-        // parse table to find rooms
+        // parse table instances to find rooms (should have an array of tables)
         // Follow links in table (href) to find more data about the rooms
         return []; // stub
     }
