33799e2c4f3c70bf957468fb15cc1cd2a968118e
Owen Sheekey <oisheekey@gmail.com>
2019-10-25 15:58:10 -0700

    Update IndexValidatorFunctions.ts

--- src/D2/data/IndexValidatorFunctions.ts
+++ src/D2/data/IndexValidatorFunctions.ts
@@ -27,9 +27,51 @@ function tablesearchrec(cnodes: NodeListOf<ChildNode>): ChildNode[] {
     return found;
 }
 
-export function validateTables(tables: ChildNode[]): ChildNode {
-    for (const table of tables) {
-        // stub
+// Attempts to add all HREF elements to new room objects, moves on to next table if there is a failure
+export function constructRooms(tables: ChildNode[]): IRoom[] {
+    for (let table of tables) {
+        // Extract some useful fields from the table
+        let thead: ChildNode;
+        let trthead: ChildNode;
+        let tbody: ChildNode;
+        try {
+            thead = findNode(table.childNodes, "thead");
+            trthead = findNode(thead.childNodes, "tr");
+            tbody = findNode(table.childNodes, "tbody");
+        } catch (error) {
+            continue;
+        }
+        // Find the relevant index for href
+        const hrefind: number = findNodeWithAttr(trthead.childNodes, "views-field views-field-title");
+        let hrefchildren: NodeListOf<ChildNode> = trthead.childNodes[hrefind].childNodes;
+        // const href: string = findNode(hrefchildren, "a").attrs[0].value;
+
     }
     return null; // stub
+}
+
+
+// Find first instance of Node with a given name in a NodeListOf<ChildNode>
+export function findNode(nodes: NodeListOf<ChildNode>, name: string): ChildNode {
+    const len: number = nodes.length;
+    for (let i = 0; i < len; i++) {
+        if (nodes[i].nodeName === name) {
+            return nodes[i];
+        }
+    }
+    throw new Error("Node not found");
+}
+
+// Returns node index with specfied attribute, returns -1 on none
+export function findNodeWithAttr (nodes: NodeListOf<ChildNode>, attr: string): number {
+    const len: number = nodes.length;
+    for (let i = 0; i < len; i++) {
+        if (!nodes[i].hasOwnProperty("attrs")) {
+            continue;
+        }
+        // if (nodes[i].attrs[0].value === attr) {
+        //     return i;
+        // }
+    }
+    return -1;
 }
